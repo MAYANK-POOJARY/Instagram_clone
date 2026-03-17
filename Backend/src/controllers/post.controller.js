6d27115db4f55a198300e882e12d3b1230998832
Mayank <poojary.mayank@gmail.com>
2026-03-17 21:29:56 +0530

    post-details-stored-to-db

--- Backend/src/controllers/post.controller.js
+++ Backend/src/controllers/post.controller.js
@@ -1,23 +1,48 @@
 const postModel = require("../models/post.model");
-const ImageKit = require( '@imagekit/nodejs');
-const { toFile } = require ('@imagekit/nodejs');
+const ImageKit = require("@imagekit/nodejs");
+const { toFile } = require("@imagekit/nodejs");
+const jwt = require("jsonwebtoken");
 
 const client = new ImageKit({
-  privateKey: process.env.IMAGEKIT_PRIVATE_KEY, // This is the default and can be omitted
+  privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
 });
-
 
 async function createPostController(req, res) {
-    console.log(req.body , req.file);
-    const file = await client.files.upload({
-         file: await toFile(Buffer.from(req.file.buffer), 'file'),
-         fileName: 'fileName',
-});
 
-res.send(file)
-    
+  const token = req.cookies.token; // token of the user
+  if (!token) {
+    return res.status(401).json({
+      message: "Token not provided , Unauthorized access.",
+    });
+  }
+
+  let decoded = null; // decoded token containing the user id will be stored in this variable
+  try {
+    decoded = jwt.verify(token, process.env.JWT_SECRET);
+  } catch (error) {
+    return res.status(401).json({
+      message : "User is not authorized."
+    })
+  }
+
+  const file = await client.files.upload({
+    file: await toFile(Buffer.from(req.file.buffer), "file"),
+    fileName: "fileName",
+    folder: "instagram_clone"
+  });
+
+
+  const post = await postModel.create({
+    caption: req.body.caption,
+    imgUrl: file.url,
+    user: decoded.id
+  })
+
+  res.status(201).json({
+    message: "Post created successfully.",
+    post
+  })
 }
-
 module.exports = {
-    createPostController
-}
+  createPostController,
+};
